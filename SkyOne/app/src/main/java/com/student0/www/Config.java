4877b5fb75ff48fda78d97316c91cba2945054e8
student084 <2946465099@qq.com>
2017-02-26 20:53:48 +0800

    Add: post can return ,but still bug

--- SkyOne/app/src/main/java/com/student0/www/Config.java
+++ SkyOne/app/src/main/java/com/student0/www/Config.java
@@ -8,10 +8,12 @@ import android.util.LruCache;
  */
 
 public class Config {
-    public final static String PHOTOS_DIR = "/sdcard/skyone";
+
     public final static int THREAD_NUMBER_URL_LOAD= 3;
     public final static int THREAD_NUMBER_LOAD_LOCAL = 2;
 
+    public final static String PHOTOS_DIR = "/sdcard/skyone";
+    public final static String UPLOAD_PHOTO_URL = "http://10.60.0.164:8080/SkyOne/servlet/UploadPhoto";
 
 
     public final static int TEMPS_FRAGMENT_POSITION_IN_VIEWPAGER = 1;
--- SkyOne/app/src/main/java/com/student0/www/util/UpLoadPhotos.java
+++ SkyOne/app/src/main/java/com/student0/www/util/UpLoadPhotos.java
@@ -3,16 +3,23 @@ package com.student0.www.util;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.provider.Settings;
+import android.util.Log;
 
 import com.student0.www.Config;
 import com.student0.www.skyone.AppList;
 import com.student0.www.skyone.MyCache;
 
 import java.io.File;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.io.IOException;
 import java.util.concurrent.Semaphore;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * Created by willj on 2017/2/25.
@@ -41,17 +48,25 @@ public class UpLoadPhotos {
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
-                            if(upload(AppList.select_photos_wait_upload.get(0))){
-                            //Remove the photo from upload list
-                            MyCache.deletePhotoName(AppList.select_photos_wait_upload.get(0));
-                            //remove the file of this photo
-                            deleteTempPhoto(new File(AppList.select_photos_wait_upload.get(0)));
-                            //remove the photo from the list which is used to create the tempPhotos
-                            AppList.select_photos_wait_upload.remove(0);
-                                semaphoreUploadThread.release();
+                            try {
+                                while (1 > 0)
+                                {
+                                    System.out.println("-----------------------------------------------");
+                                if(upload(AppList.select_photos_wait_upload.get(0))) {
+                                    //Remove the photo from upload list
+                                    MyCache.deletePhotoName(AppList.select_photos_wait_upload.get(0));
+                                    //remove the file of this photo
+                                    deleteTempPhoto(new File(AppList.select_photos_wait_upload.get(0)));
+                                    //remove the photo from the list which is used to create the tempPhotos
+                                    AppList.select_photos_wait_upload.remove(0);
+                                    semaphoreUploadThread.release();
+                                }
 
-                    }
-                }
+                        }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
                     }
                 };
                 Looper.loop();
@@ -71,8 +86,32 @@ public class UpLoadPhotos {
      * Upload pic
      * if success return true, else return false
      * */
-    private boolean upload(String s) {
-        return true;
+    public static final MediaType MEDIA_TYPE_PNG =
+            MediaType.parse("image/png");
+
+    private final OkHttpClient client = new OkHttpClient();
+
+    private boolean upload(String s) throws Exception{
+        File file = new File(s);
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("batch", "1")
+                .addFormDataPart("image", s,
+                        RequestBody.create(MEDIA_TYPE_PNG, file))
+                .build();
+        System.out.println(requestBody.toString());
+        Request request = new Request.Builder()
+                .url(Config.UPLOAD_PHOTO_URL)
+                .post(requestBody)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        if (response.isSuccessful()) {
+            return true;
+        };
+        System.out.println(response.body().toString());
+
+        return false;
     }
 
     public static UpLoadPhotos getInstance(){
